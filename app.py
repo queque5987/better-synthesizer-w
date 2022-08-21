965d04b76fc9b57e6f8558354c61f1951a3749fc
queque5987 <pyw5987@gmail.com>
2022-08-22 06:32:33 +0900

    20220822 0632

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import numpy as np
 import torch
 import os
 
+import rtvc_main
+
 app = FastAPI()
+
+class UserInput(BaseModel):
+    chars: list
+    batched_embeds: list
 
 @app.get('/')
 def index():
     return {"Message": "This is Index"}
 
-@app.get('/load/')
-async def load():
-    path = "model/"
-    ckpt = []
-    for pt in os.listdir(path):
-        tensor = torch.load(os.path.join(path, pt))
-        ckpt.append(pt[4:-3])
-        ckpt.append(tensor.tolist())
-    ckpt = jsonable_encoder(ckpt)
-    return JSONResponse(ckpt)
+@app.get('/inference/')
+async def inference(userinput: UserInput):
+    _model = rtvc_main.tacotron()
+    userinput = userinput.dict()
+    chars = userinput["chars"]
+    chars = np.stack(chars)
+    chars = torch.tensor(chars).long().to('cpu')
+
+    batched_embeds = userinput["batched_embeds"]
+    speaker_embeds = np.stack(batched_embeds)
+    speaker_embeddings = torch.tensor(speaker_embeds).float().to('cpu')
+
+    _, mel, alignments = _model.generate(chars, speaker_embeddings)
+    mel = mel.tolist()
+    alignments = alignments.tolist()
+    rsp = jsonable_encoder((mel, alignments))
+    return JSONResponse(rsp)
---
+++ rtvc_main.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from models import tacotron
+from hparams import hparams
+from symbols import symbols
+import collections
+import os
+import torch
+
+class tacotron:
+    def __init__(self):
+        self._model = tacotron.Tacotron(
+            embed_dims=hparams.tts_embed_dims,
+            num_chars=len(symbols),
+            encoder_dims=hparams.tts_encoder_dims,
+            decoder_dims=hparams.tts_decoder_dims,
+            n_mels=hparams.num_mels,
+            fft_bins=hparams.num_mels,
+            postnet_dims=hparams.tts_postnet_dims,
+            encoder_K=hparams.tts_encoder_K,
+            lstm_dims=hparams.tts_lstm_dims,
+            postnet_K=hparams.tts_postnet_K,
+            num_highways=hparams.tts_num_highways,
+            dropout=hparams.tts_dropout,
+            stop_threshold=hparams.tts_stop_threshold,
+            speaker_embedding_size=hparams.speaker_embedding_size).to('cpu')
+
+    def load_model(self):
+        path = Path("checkpoint/")
+        checkpoint = collections.OrderedDict()
+        for pt in os.listdir(path):
+            tensor = torch.load(os.path.join(path, pt))
+            checkpoint[pt[4:-3]] = tensor
+        self._model.model_state_load(checkpoint)
+
+    def generate(self, chars, speaker_embeddings):
+        return self.generate(chars, speaker_embeddings)
+
+# if __name__ == "__main__":
+#     embed = api_test.get_embed()
+#     spec = inference(embed, "I love you")
+#     print(spec)
+    # args = rtvc_args()
+    # synthesizer = Synthesizer(args.syn_model_fpath)
+    # text = ""
+    # embed = []
+    # # The synthesizer works in batch, so you need to put your data in a list or numpy array
+    # texts = [text]
+    # embeds = [embed]
+    # # If you know what the attention layer alignments are, you can retrieve them here by
+    # # passing return_alignments=True
+    # specs = synthesizer.synthesize_spectrograms(texts, embeds)
+    # spec = specs[0]
+    # print("Created the mel spectrogram")
+    # ## Generating the waveform
+    # print("Synthesizing the waveform:")
