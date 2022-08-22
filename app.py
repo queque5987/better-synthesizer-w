6e999874d3c7d6a8ea5f6aa206a73cc6965b4083
queque5987 <pyw5987@gmail.com>
2022-08-22 16:30:59 +0900

    20220822 1630

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ async def generate(userinput: UserInput):
     print("--mel--")
     mel = mel.tolist()
     # alignments = alignments.tolist()
-    mel = jsonable_encoder(mel[0])
+    mel = jsonable_encoder(mel)
     return JSONResponse(mel)
     #     {'mel': mel,
     #     'alignments': alignments
